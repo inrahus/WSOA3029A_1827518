39a51e569132a299b50cb5fc0c0197be4a52ac7e
inrahus <60891951+inrahus@users.noreply.github.com>
2020-07-21 12:41:27 +0200

    Created Sidebar Responsive Menu
    Sidebar menu for mible and smaller screens

---
+++ Hub/JavaScript/responsive.js
@@ -0,0 +1,27 @@
+const slide = () => {
+    const menuicon = document.querySelector('.menuicon');
+    const nav = document.querySelector('.nav-links');
+    const links = document.querySelectorAll('.nav-links li');
+
+    //Toggle Nav
+    menuicon.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        //Make links appear in delay (animation)
+        links.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`
+            }
+        });
+
+        //menuicon animation
+        menuicon.classList.toggle('toggle');
+    })
+
+
+}
+
+slide();
