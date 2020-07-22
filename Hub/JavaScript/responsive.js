6dab5f3847ba63667398a9f9d17536801a46d59e
inrahus <60891951+inrahus@users.noreply.github.com>
2020-07-22 10:28:43 +0200

    Fixed Responsive Nav for Mobile
    Added rainbow hover underline that works on mobile side menu

--- Hub/JavaScript/responsive.js
+++ Hub/JavaScript/responsive.js
@@ -6,7 +6,6 @@ const slide = () => {
     //Toggle Nav
     menuicon.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-
         //Make links appear in delay (animation)
         links.forEach((link, index) => {
             if (link.style.animation) {
@@ -16,12 +15,9 @@ const slide = () => {
                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`
             }
         });
-
         //menuicon animation
         menuicon.classList.toggle('toggle');
     })
-
-
 }
 
 slide();
