92a578c011a7a7f92f650d14c66888571e07412d
inrahus <60891951+inrahus@users.noreply.github.com>
2020-10-23 17:52:53 +0200

    New Graph Finally

--- Hub/JavaScript/TotalDeathsChart.js
+++ Hub/JavaScript/TotalDeathsChart.js
@@ -1,131 +1,61 @@
-const fetchStuff = async (url) => {
-    const response = await fetch(url);
-    return await response.text();
-}
 
+var width = 1000,
+    height = 600;
 
+var colors = d3.scaleOrdinal(d3.schemeTableau10);
 
+var svg = d3.select("#my_viz")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("background", "black");
 
+var details = [
+    { status: "Deaths: 5", number: 5 },
+    { status: "Recovered Cases: 31", number: 31 },
+    { status: "Active Cases: 1317", number: 1317 }
+]
 
+var data = d3.pie().sort(null).value(function (d) { return d.number; })(details);
+console.log(data);
 
+var slices = d3.arc()
+    .innerRadius(0)
+    .outerRadius(225)
+    .padAngle(0.0)
+    .padRadius(50);
 
+var toppings = svg.append("g").attr("transform", "translate(300, 300)")
+    .selectAll("path").data(data);
 
+toppings.enter().append("path").attr("d", slices)
+    .attr("fill", function (d) {
+        return colors(d.data.number);
+    });
 
+var legends = svg
+    .append("g")
+    .attr("transform", "translate(650, 175)")
+    .selectAll(".legends").data(data);
 
+var legend = legends.enter()
+    .append("g")
+    .classed("legends", true)
+    .attr("transform", function (d, i) {
+        return "translate(0," + (i + 1) * 50 + ")";
+    });
 
+legend
+    .append("rect")
+    .attr("width", 30)
+    .attr("height", 30)
+    .attr("fill", function (d) {
+        return colors(d.data.number);
+    });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//PREVIOUS CHART JS
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: ['13', '14', '15', '16', '17', '18', '19', '10', '21', '22', '23', '24', '25', '26', '27', '28',
-//         ],
-//         datasets: [{
-//             label: 'Total Number of Deaths',
-//             data: [25, 27, 27, 34, 48, 50, 52, 54, 58, 58, 65, 75, 79, 86, 87, 90],
-//             backgroundColor: [
-//                 'rgba(0, 0, 0, 0.9)'],
-//             borderColor: [
-//                 'rgba(0, 0, 0, 1)'],
-//             borderWidth: 1,
-//             hoverBorderWidth: 5,
-//             hoverBorderColor: 'white'
-//         }]
-//     },
-//     options: {
-//         title: {
-//             display: true,
-//             text:
-//                 'Total Number of COVID Deaths from May 14th to May 28th in South Africa',
-//             fontSize: 35,
-//             fontColor: 'white'
-//         },
-
-//         legend: {
-//             display: false,
-//             position: 'right',
-//             labels: {
-//                 fontColor: 'white'
-//             }
-//         },
-//         layout: {
-//             padding: {
-//                 left: 50,
-//                 right: 50,
-//                 top: 50,
-//                 bottom: 50
-//             }
-//         },
-
-//         scales: {
-//             xAxes: [{
-//                 scaleLabel: {
-//                     display: true,
-//                     labelString: 'Date in May 2020',
-//                     fontSize: 20,
-//                     fontColor: 'white'
-//                 },
-
-//                 gridLines: {
-//                     display: true,
-//                     color: "white"
-//                 },
-
-//                 ticks: {
-//                     fontColor: "white",
-//                 }
-//             }],
-
-//             yAxes: [{
-//                 scaleLabel: {
-//                     display: true,
-//                     labelString: 'Total Number of Deaths',
-//                     fontSize: 20,
-//                     fontColor: 'white'
-//                 },
-
-//                 gridLines: {
-//                     display: true,
-//                     color: "white"
-//                 },
-
-//                 ticks: {
-//                     fontColor: "white",
-//                 }
-//             }]
-//         }
-
-//     }
-
-// });
+legend.append("text")
+    .text(function (d) { return d.data.status; })
+    .attr("fill", function (d) { return colors(d.data.number); })
+    .attr("x", 40)
+    .attr("y", 25)
+    .attr("font-size", 30);
